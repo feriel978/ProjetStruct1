c758141976353cbc73e68612cf61643f791f53a8
feriel978 <abalacheferiel@gmail.com>
2024-09-20 09:25:35 +0200

    Ajout de branche

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-        greeting = 'hi';
+        greeting = 'enter';
         changeHandler(event) {
         this.greeting = event.target.value;
         }
