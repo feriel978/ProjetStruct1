3b015795c4e399cc68278811b2c9961b62269094
feriel978 <abalacheferiel@gmail.com>
2024-09-25 15:03:24 +0200

    updateFinal

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-        greeting = 'Enter UserName please';
+        greeting = 'Eter UserName please';
         changeHandler(event) {
         this.greeting = event.target.value;
         }
